7b2b49e204741a04b3885373d78d92ea3c26c379
Serhii Lekhman <8.lsv.888@gmail.com>
2024-04-07 11:06:47 +0200

    add api.js

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,17 +1,18 @@
-import { Layout } from 'antd';
+import { Layout, Card } from 'antd';
 const { Sider } = Layout;
 
 const siderStyle = {
-  textAlign: 'center',
-  lineHeight: '120px',
-  color: '#fff',
-  backgroundColor: '#1677ff',
+  padding: '1rem',
 };
 
 const AppSider = () => {
 	return (
 		<Sider width="25%" style={siderStyle}>
-			Sider
+			<Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
 		</Sider>
 	)
 }
