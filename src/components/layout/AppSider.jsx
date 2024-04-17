57db0e9cdc3f640e18efd0b9f056dcd06bfbba41
Serhii Lekhman <8.lsv.888@gmail.com>
2024-04-17 21:15:29 +0200

    update AppSider: add List

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,4 +1,4 @@
-import { Layout, Card, Statistic } from "antd";
+import { Layout, Card, Statistic, List, Typography } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 const { Sider } = Layout;
 
@@ -6,10 +6,18 @@ const siderStyle = {
   padding: "1rem",
 };
 
+const data = [
+  "Racing car sprays burning fuel into crowd.",
+  "Japanese princess to wed commoner.",
+  "Australian walks 100km after outback crash.",
+  "Man charged over missing wedding girl.",
+  "Los Angeles battles huge wildfires.",
+];
+
 const AppSider = () => {
   return (
     <Sider width="25%" style={siderStyle}>
-      <Card style={{marginBottom: '1rem'}}>
+      <Card style={{ marginBottom: "1rem" }}>
         <Statistic
           title="Active"
           value={11.28}
@@ -18,13 +26,21 @@ const AppSider = () => {
           prefix={<ArrowUpOutlined />}
           suffix="%"
         />
+        <List
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item>
+              <Typography.Text mark>[ITEM]</Typography.Text> {item}
+            </List.Item>
+          )}
+        />
       </Card>
       <Card>
-			<Statistic
+        <Statistic
           title="Idle"
           value={9.3}
           precision={2}
-          valueStyle={{ color: '#cf1322' }}
+          valueStyle={{ color: "#cf1322" }}
           prefix={<ArrowDownOutlined />}
           suffix="%"
         />
